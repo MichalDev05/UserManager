f2758ea6ae7f116d836405226866ed8ad00bebb9
MichalFrater <miskofrater@gmail.com>
2023-09-08 00:06:07 +0200

    removed comments

--- UserManager/src/Application.h
+++ UserManager/src/Application.h
@@ -1,9 +1,7 @@
 #pragma once
 
-//Vrátaj všetky include files/headery
 #include <iostream>
 #include <vector>
 #include "User.h"
 
-//definuj funkciu
 int LogIn(Application::User& user);
--- UserManager/src/User.h
+++ UserManager/src/User.h
@@ -4,33 +4,30 @@
 namespace Application {
 	class User {
 	public:
-		//Konštruktor, berie meno a heslo, kroré nastaví novému userovi
 		User(std::string password, std::string username, std::string role = "user") {
 			this->m_Password = password;
 			this->m_Username = username;
 			this->m_Role = role;
 		}
 
-		//Funkcia login, ktorá vráti true ak sa s userom zhodujú login parametre
 		bool Login(std::string username, std::string password) {
 			return (this->m_Password == password && this->m_Username == username);
 		}
-		//Funkcia, ktorá vráti meno
+
 		std::string GetUsername() {
 			return m_Username;
 		}
-		//Funkcia, ktorá vráti rolu
+
 		std::string GetRole() {
 			return m_Role;
 		}
-		//Funkcia, ktorá zmení heslo a vždy vráti 1, èo je v main definované ako úspech
+
 		int ChangePassword(std::string password) {
 			this->m_Password = password;
 			return 1;
 		}
 
 	private:
-		//Údaje usera
 		std::string m_Password;
 		std::string m_Username;
 		std::string m_Role;
